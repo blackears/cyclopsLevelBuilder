c58a51f107a6e7bc375f4f06229781d45b6e8377
Mark McKay <mark@kitfox.com>
2023-07-09 11:04:56 -0400

    Adding option to export a flat file.

--- addons/cyclops_level_builder/exporter/exporter_gltf_wizard.gd
+++ addons/cyclops_level_builder/exporter/exporter_gltf_wizard.gd
@@ -33,6 +33,8 @@ var save_path:String
 
 var plugin:CyclopsLevelBuilder
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
@@ -64,6 +66,17 @@ func _on_bn_browse_pressed():
 	file_dialog.popup_centered()
 	pass
 
+func branch_is_valid(node:Node)->bool:
+	if node is CyclopsBlock || (%check_markers.button_pressed && node is Marker3D):
+		return true
+	
+	for child in node.get_children():
+		if child is Node3D and branch_is_valid(child):
+			return true
+	
+	return false
+	
+
 func clean_branch(node:Node3D)->Node3D:
 	if node is CyclopsBlock:
 		var block:CyclopsBlock = node
@@ -76,24 +89,73 @@ func clean_branch(node:Node3D)->Node3D:
 		new_node.add_child(new_mesh_node)
 		return new_node
 		
+	elif node is Marker3D:
+		var new_node:Marker3D = node.duplicate()
+		return new_node
+		
 	else:
 		var new_node:Node3D = Node3D.new()
 		new_node.transform = node.transform
 		new_node.name = node.name
 		for child in node.get_children():
-			if child is Node3D:
+			if branch_is_valid(child):
 				new_node.add_child(clean_branch(child))
 		return new_node
+
+
+func search_nodes_flat(node:Node, root:Node3D):
+#	print("searching %s" % node.name)
+	
+	if node is CyclopsBlock:
+		#print("exporting block %s" % node.name)
+		var block:CyclopsBlock = node
+		var new_mesh_node:MeshInstance3D = block.mesh_instance.duplicate()
+		new_mesh_node.name = block.name
+		
+		root.add_child(new_mesh_node)
+		new_mesh_node.global_transform = block.mesh_instance.global_transform
+		
+	elif node is Marker3D:
+		if %check_markers.button_pressed:
+			var new_node:Marker3D = Marker3D.new()
+			new_node.name = node.name
+
+			root.add_child(new_node)
+			new_node.global_transform = node.global_transform
+
+		
+	for child in node.get_children():
+		search_nodes_flat(child, root)
+	
+	
+
+func clean_flat(node:Node3D)->Node3D:
+	#print("clean_flat")
+	var root:Node3D = Node3D.new()
+	root.name = "CyclopsScene"
+	
+	for child in node.get_children():
+		#print("rpt chjild %s" % child.name)
+		search_nodes_flat(child, root)
+		
+	return root
+	
 
 func _on_bn_okay_pressed():
 	var exporter:CyclopsExporterGltf = CyclopsExporterGltf.new()
 	
+	var path:String = save_path
+	if !save_path.to_lower().ends_with(".gltf") && !save_path.to_lower().ends_with(".glb"):
+		path = save_path + ".gltf"
+		
+	
 	var doc:GLTFDocument = GLTFDocument.new()
 	var state:GLTFState = GLTFState.new()
 	var root:Node = plugin.get_editor_interface().get_edited_scene_root()
-	var root_clean:Node3D = clean_branch(root)
+	var root_clean:Node3D = clean_flat(root) if %check_flatten.button_pressed else clean_branch(root)
+	
 	doc.append_from_scene(root_clean, state)
-	doc.write_to_filesystem(state, save_path)
+	doc.write_to_filesystem(state, path)
 	
 	hide()
 
