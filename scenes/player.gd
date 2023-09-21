dfa9a45cc9a3895e13c4dcb7f732d4038f835e3b
Mark McKay <mark@kitfox.com>
2023-09-21 07:50:20 -0400

    Modifying player for turning left and right.

--- scenes/player.gd
+++ scenes/player.gd
@@ -19,8 +19,17 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var x_axis:float = Input.get_axis("ui_right", "ui_left")
+	var y_axis:float = Input.get_axis("ui_down", "ui_up")
+	
+	var basis:Basis = transform.basis
+	basis = basis.rotated(Vector3.UP, delta * x_axis * 1)
+	transform.basis = basis
+	
+	var direction = basis.z * y_axis
+	
+#	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+#	var direction = (transform.basis * Vector3(-input_dir.x, 0, -input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
