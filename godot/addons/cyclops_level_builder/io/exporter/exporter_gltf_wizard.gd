a5e1e03361005455f23d63a9cbce9f3d33a2573d
kitfox <mark@kitfox.com>
2025-09-30 01:35:18 -0400

    Fixing gltf export.
    https://github.com/blackears/cyclopsLevelBuilder/issues/35

--- godot/addons/cyclops_level_builder/io/exporter/exporter_gltf_wizard.gd
+++ godot/addons/cyclops_level_builder/io/exporter/exporter_gltf_wizard.gd
@@ -25,49 +25,48 @@
 extends Window
 class_name ExporterGltfWizard
 
-#@onready var _text_path:LineEdit = $VBoxContainer/HBoxContainer/text_path
-#var _text_path:LineEdit
-
-var file_dialog:FileDialog
+#var file_dialog:FileDialog
 var save_path:String
 
 var plugin:CyclopsLevelBuilder
 
-
+@onready var check_flatten:CheckBox = %check_flatten
+@onready var check_markers:CheckBox = %check_markers
+@onready var check_lights:CheckBox = %check_lights
+@onready var check_cameras:CheckBox = %check_cameras
+@onready var check_exclude_invisible:CheckBox = %check_exclude_invisible
+@onready var lineEdit_path:LineEdit = %lineEdit_path
+@onready var file_dialog:FileDialog = %FileDialog
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
-	file_dialog = FileDialog.new()
-	add_child(file_dialog)
-	file_dialog.size = Vector2(600, 400)
-	file_dialog.file_mode = FileDialog.FILE_MODE_SAVE_FILE
-	file_dialog.set_access(FileDialog.ACCESS_FILESYSTEM)
-	file_dialog.title = "Export scene..."
-	file_dialog.filters = PackedStringArray(["*.gltf; glTF files"])
-	file_dialog.current_file = save_path
-	file_dialog.file_selected.connect(on_save_file)
+	#file_dialog = FileDialog.new()
+	#add_child(file_dialog)
+	#file_dialog.size = Vector2(600, 400)
+	#file_dialog.file_mode = FileDialog.FILE_MODE_SAVE_FILE
+	#file_dialog.set_access(FileDialog.ACCESS_FILESYSTEM)
+	#file_dialog.title = "Export scene..."
+	#file_dialog.filters = PackedStringArray(["*.gltf; glTF files"])
+#	file_dialog.file_selected.connect(on_save_file)
+#	file_dialog.current_file = save_path
 
-#	_text_path = $VBoxContainer/HBoxContainer/lineEdit_path
-	#var hh = get_node("VBoxContainer")
-	#var children = get_children()
-
-
-#	_text_path = get_node("VBoxContainer/HBoxContainer/lineEdit_path")
-	%lineEdit_path.text = save_path
-	#_text_path = %lineEdit_path
-	#_text_path.text = save_path
+#	lineEdit_path.text = save_path
+	pass
 
 
-func on_save_file(path:String):
-	save_path = path
-	%lineEdit_path.text = path
+#func on_save_file(path:String):
+	#save_path = path
+	#lineEdit_path.text = path
 
 func _on_bn_browse_pressed():
 	file_dialog.popup_centered()
 
 func branch_is_valid(node:Node)->bool:
-	if node is CyclopsBlock || (%check_markers.button_pressed && node is Marker3D):
+	if !node:
+		return false
+		
+	if node is CyclopsBlock || (check_markers.button_pressed && node is Marker3D):
 		return true
 	
 	for child in node.get_children():
@@ -77,66 +76,149 @@ func branch_is_valid(node:Node)->bool:
 	return false
 	
 
-func clean_branch(node:Node3D)->Node3D:
+func clone_branch(node:Node3D)->Node3D:
+	var node_list:Array[Node3D] = []
+	var root_clone:Node3D = clone_branch_recursive(node, node_list)
+	
+	for node_clone in node_list:
+		if node_clone != root_clone:
+#			print("setting owner ", node_clone.name)
+			node_clone.owner = root_clone
+	
+	return root_clone
+
+func clone_branch_recursive(node:Node3D, node_list:Array[Node3D])->Node3D:
+	#print("clean_branch ", node.name)
+	if check_exclude_invisible.button_pressed:
+		if !node.visible:
+			return null
+	
 	if node is CyclopsBlock:
 		var block:CyclopsBlock = node
-		var new_mesh_node:MeshInstance3D = block.mesh_instance.duplicate()
-		new_mesh_node.name = block.mesh_instance.name
-		
-		var new_node:Node3D = Node3D.new()
-		new_node.name = node.name
-		new_node.transform = node.transform
-		new_node.add_child(new_mesh_node)
+		var new_node:MeshInstance3D = block.mesh_instance.duplicate()
+		new_node.name = block.name
+		new_node.transform = block.transform
+		node_list.append(new_node)
 		return new_node
 		
 	elif node is Marker3D:
-		var new_node:Marker3D = node.duplicate()
-		return new_node
+		if check_markers.button_pressed:
+			var new_node:Marker3D = node.duplicate()
+			node_list.append(new_node)
+			new_node.transform = node.transform
+			return new_node
+		return null
+		
+	elif node is Light3D:
+		if check_lights.button_pressed:
+			var new_node:Marker3D = node.duplicate()
+			node_list.append(new_node)
+			new_node.transform = node.transform
+			return new_node
+		return null
+		
+	elif node is Camera3D:
+		if check_cameras.button_pressed:
+			var new_node:Camera3D = node.duplicate()
+			node_list.append(new_node)
+			new_node.transform = node.transform
+			return new_node
+		return null
 		
 	else:
 		var new_node:Node3D = Node3D.new()
 		new_node.transform = node.transform
 		new_node.name = node.name
+		node_list.append(new_node)
+		
 		for child in node.get_children():
 			if branch_is_valid(child):
-				new_node.add_child(clean_branch(child))
+				var child_branch_copy:Node3D = clone_branch_recursive(child, node_list)
+				new_node.add_child(child_branch_copy)
+				
 		return new_node
 
 
-func search_nodes_flat(node:Node, root:Node3D):
-#	print("searching %s" % node.name)
+#func search_nodes_flat(node:Node, root:Node3D):
+##	print("searching %s" % node.name)
+	#
+	#if node is CyclopsBlock:
+		##print("exporting block %s" % node.name)
+		#var block:CyclopsBlock = node
+		#var new_mesh_node:MeshInstance3D = block.mesh_instance.duplicate()
+		#new_mesh_node.name = block.name
+		#
+		#root.add_child(new_mesh_node)
+		#new_mesh_node.global_transform = block.mesh_instance.global_transform
+		#
+	#elif node is Marker3D:
+		#if check_markers.button_pressed:
+			#var new_node:Marker3D = Marker3D.new()
+			#new_node.name = node.name
+#
+			#root.add_child(new_node)
+			#new_node.global_transform = node.global_transform
+#
+		#
+	#for child in node.get_children():
+		#search_nodes_flat(child, root)
+	
+	
+
+func clone_flat_recursive(node:Node3D, root_clone:Node3D)->void:
+	#print("clean_branch ", node.name)
+	if check_exclude_invisible.button_pressed:
+		return
 	
 	if node is CyclopsBlock:
-		#print("exporting block %s" % node.name)
 		var block:CyclopsBlock = node
-		var new_mesh_node:MeshInstance3D = block.mesh_instance.duplicate()
-		new_mesh_node.name = block.name
-		
-		root.add_child(new_mesh_node)
-		new_mesh_node.global_transform = block.mesh_instance.global_transform
+		var new_node:MeshInstance3D = block.mesh_instance.duplicate()
+		new_node.name = block.name
+		root_clone.add_child(new_node)
+		new_node.owner = root_clone
+		new_node.transform = node.global_transform
+		return
 		
 	elif node is Marker3D:
-		if %check_markers.button_pressed:
-			var new_node:Marker3D = Marker3D.new()
-			new_node.name = node.name
-
-			root.add_child(new_node)
-			new_node.global_transform = node.global_transform
-
+		if check_markers.button_pressed:
+			var new_node:Marker3D = node.duplicate()
+			root_clone.add_child(new_node)
+			new_node.owner = root_clone
+			new_node.transform = node.global_transform
+			return
+		return
 		
-	for child in node.get_children():
-		search_nodes_flat(child, root)
-	
-	
+	elif node is Light3D:
+		if check_lights.button_pressed:
+			var new_node:Marker3D = node.duplicate()
+			root_clone.add_child(new_node)
+			new_node.owner = root_clone
+			new_node.transform = node.global_transform
+			return
+		return
+		
+	elif node is Camera3D:
+		if check_cameras.button_pressed:
+			var new_node:Camera3D = node.duplicate()
+			root_clone.add_child(new_node)
+			new_node.owner = root_clone
+			new_node.transform = node.global_transform
+			return
+		return
+		
+	else:
+		for child in node.get_children():
+			if branch_is_valid(child):
+				clone_flat_recursive(child, root_clone)
 
-func clean_flat(node:Node3D)->Node3D:
+func clone_flat(node:Node3D)->Node3D:
 	#print("clean_flat")
 	var root:Node3D = Node3D.new()
-	root.name = "CyclopsScene"
+	root.name = "root"
 	
 	for child in node.get_children():
 		#print("rpt chjild %s" % child.name)
-		search_nodes_flat(child, root)
+		clone_flat_recursive(child, root)
 		
 	return root
 	
@@ -146,12 +228,16 @@ func _on_bn_okay_pressed():
 	var path:String = save_path
 	if !save_path.to_lower().ends_with(".gltf") && !save_path.to_lower().ends_with(".glb"):
 		path = save_path + ".gltf"
-		
 	
 	var doc:GLTFDocument = GLTFDocument.new()
 	var state:GLTFState = GLTFState.new()
 	var root:Node = plugin.get_editor_interface().get_edited_scene_root()
-	var root_clean:Node3D = clean_flat(root) if %check_flatten.button_pressed else clean_branch(root)
+	var root_clean:Node3D
+	
+	if check_flatten.button_pressed:
+		root_clean = clone_flat(root)
+	else:
+		root_clean = clone_branch(root)
 	
 	doc.append_from_scene(root_clean, state)
 	doc.write_to_filesystem(state, path)
@@ -165,3 +251,9 @@ func _on_bn_cancel_pressed():
 
 func _on_close_requested():
 	hide()
+
+
+func _on_file_dialog_file_selected(path: String) -> void:
+	save_path = path
+	lineEdit_path.text = path
+	pass # Replace with function body.
